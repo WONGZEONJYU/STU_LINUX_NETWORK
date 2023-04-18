4bb359b6978880435f62b5cebaa8f39eadf17994
WONGZEONJYU <958323726@qq.com>
2023-04-18 16:55:39 +0800

    edit

---
+++ 8.WinSock/Client.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+
+using namespace std;
+
+int main()
+{
+    WSADATA wd{};
+
+    if (WSAStartup(MAKEWORD(2,2) , &wd)){
+        cout << "startup error" << endl;
+        return -1;
+    }
+
+    SOCKET sock{ socket(PF_INET,SOCK_STREAM,IPPROTO_TCP) };
+
+    if (INVALID_SOCKET == sock) {
+        cout << "socket error" << endl;
+        return -1;
+    }
+
+    sockaddr_in addr{};
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    addr.sin_port = htons(8888);
+
+    if (SOCKET_ERROR == connect(sock, reinterpret_cast<const sockaddr*>(&addr), sizeof(addr))) {
+        cout << "connect error" << endl;
+        return -1;
+    }
+
+    cout << "connect success sock :" << sock << endl;
+
+    for (;;) {
+
+        char input[32]{}, buf[128]{};
+
+        cout << "Input: " << endl;
+
+        cin >> input;
+
+        int len(send(sock, input, strlen(input) + 1, 0));
+
+        int r(recv(sock, buf, sizeof(buf), 0));
+
+        if (r > 0) {
+            cout << "Receive :" << buf << endl;
+        }
+        else {
+            cout << "client quit r :" << r << endl;
+            break;
+        }
+    }
+
+    closesocket(sock);
+
+    WSACleanup();
+
+    return 0;
+}
+
---
+++ 8.WinSock/Server.cpp
@@ -0,0 +1,86 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    WSADATA wd{};
+
+    if (WSAStartup(MAKEWORD(2,2),&wd)){
+        cout << "startup error" << endl;
+        return -1;
+    }
+
+    SOCKET server{ socket(PF_INET,SOCK_STREAM,IPPROTO_TCP) };
+
+    if (INVALID_SOCKET == server) {
+        cout << "server socket error" << endl;
+        return -1;
+    }
+
+    sockaddr_in saddr{};
+
+    saddr.sin_family = AF_INET;
+    saddr.sin_addr.s_addr = htonl(INADDR_ANY);//htonl函数把小端转换成大端（网络字节序采用大端）
+    saddr.sin_port = htons(8888);
+
+    if (SOCKET_ERROR == bind(server, reinterpret_cast<const sockaddr*>(&saddr), sizeof(saddr))) {
+        cout << "server bind error" << endl;
+        return -1;
+    }
+
+    if (SOCKET_ERROR == listen(server, 1)) {
+        cout << "server listen error" << endl;
+        return -1;
+    }
+
+    cout << "server start success" << endl;
+
+    while (true) {
+
+        sockaddr_in caddr{};
+        int asize{ sizeof(caddr) };
+
+        SOCKET client{ accept(server,reinterpret_cast<sockaddr*>(&caddr),&asize) };
+
+        if (INVALID_SOCKET == client) {
+            cout << "client accept success" << endl;
+            return -1;
+        }
+
+        cout << "client :" << client << endl; //client的数值表示系统资源的id
+
+        int r{}, len{};
+
+        do {
+            char buf[32]{};
+
+            r = recv(client, buf, (sizeof(buf) / sizeof(char)), 0);
+
+            if (r > 0) {
+
+                cout << "Server Receive :" << buf << endl;
+
+                if (strcmp(buf, "quit")) {
+
+                    len = send(client, buf, r, 0);
+                }
+                else {
+                    break;
+                }
+            }
+
+        } while (r > 0);
+
+        closesocket(client);
+    }
+
+    closesocket(server);
+
+    WSACleanup();
+
+    return 0;
+}
+
