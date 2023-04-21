4a1510206bbe327cf4a7b3415c82b1502cb3c0fa
WONGZEONJYU <958323726@qq.com>
2023-04-21 09:09:00 +0800

    add

---
+++ 10.protocol/message.h
@@ -0,0 +1,25 @@
+#pragma once
+#ifndef MESSAGE_H
+#define MESSAGE_H
+
+struct Message
+{
+    unsigned short type;
+    unsigned short cmd;
+    unsigned short index;
+    unsigned short total;
+    unsigned int length;
+    unsigned char payload[];
+};
+
+Message * Message_New(unsigned short type,
+                    unsigned short cmd,
+                    unsigned short index,
+                    unsigned short total,
+                    const char * payload,
+                    unsigned int length);
+
+
+
+
+#endif // MESSAGE_H
