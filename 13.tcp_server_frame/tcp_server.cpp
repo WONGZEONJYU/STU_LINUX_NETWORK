5da82a313560da6668d8656286df055b20beb510
WONGZEONJYU <958323726@qq.com>
2023-04-28 00:37:29 +0800

    add

---
+++ 13.tcp_server_frame/tcp_server.cpp
@@ -0,0 +1,97 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <cstdio>
+#include <unistd.h>
+#include <cstring>
+#include <malloc.h>
+#include "tcp_server.h"
+
+#define FD_SIZE FD_SETSIZE
+
+struct Server
+{
+    int fd;    //服务端socket
+    int valid;    //服务端是否有效
+    Listener cb;    //回调函数
+    TcpClient * client[FD_SIZE];   //存放通信客户端数组
+};
+
+TcpServer * TcpServer_New()
+{
+    Server * ret { reinterpret_cast<Server *>( malloc(sizeof(Server)) ) };
+ 
+    if (ret) {
+
+        ret->fd = -1;
+
+        ret->valid = 0;
+
+        ret->cb = nullptr;
+    
+        for (int i {}; i < FD_SIZE; i++){
+
+            ret->client[i] = nullptr;
+        }
+    }
+
+    return ret;
+}
+
+int TcpServer_Start(TcpServer * server,int port,int max)
+{
+    int ret{};
+
+    Server * s { reinterpret_cast<Server *>(server) };
+
+    if (s && (!s->valid)){
+        
+        s->fd = socket(PF_INET,SOCK_STREAM,0);
+
+        s->valid = (-1 != s->fd);
+
+        sockaddr_in saddr {};
+
+        saddr.sin_family = AF_INET;
+
+        saddr.sin_addr.s_addr = htonl(INADDR_ANY);//htonl函数把小端转换成大端（网络字节序采用大端）
+
+        saddr.sin_port = htons(port);
+
+        s->valid = s->valid && bind( s->fd,reinterpret_cast<const sockaddr *>(&saddr),sizeof(saddr) );
+
+        s->valid = s->valid && listen(s->fd,1);
+
+    }
+    
+    return ret;
+}
+
+void TcpServer_Stop(TcpServer * server)
+{
+    Server * s {reinterpret_cast<Server *>(server)};
+}
+
+void TcpServer_SetListener(TcpServer * server,Listener listener)
+{
+    
+}
+
+int TcpServer_IsValid(TcpServer * server)
+{
+    int ret {};
+
+    return ret;
+}
+
+void TcpServer_DoWork(TcpServer * server)
+{
+
+}
+
+void TcpServer_Del(TcpServer * server)
+{
+
+}
---
+++ 13.tcp_server_frame/tcp_server.h
@@ -0,0 +1,32 @@
+#ifndef _TCP_SERVER_H
+#define _TCP_SERVER_H
+
+#include "tcp_client.h"
+
+typedef void TcpServer;
+typedef void (*Listener)(TcpClient * ,int);
+//第一个参数是用于通信的Client(该类型在客户端通信框架有说明)
+//第二个参数是事件值,用于标识事件类型
+
+enum    //事件类型定义
+{
+    EVT_CONN,    //连接事件
+    EVT_DATA,    //数据事件
+    EVT_CLOSE,    //断连事件
+};
+
+TcpServer * TcpServer_New();
+
+int TcpServer_Start(TcpServer * server,int port,int max);    //开始监听
+
+void TcpServer_Stop(TcpServer * server);    //停止监听
+
+void TcpServer_SetListener(TcpServer * server,Listener listener);    //设置回调函数
+
+int TcpServer_IsValid(TcpServer * server);    //检查服务端是否有效
+
+void TcpServer_DoWork(TcpServer * server);    //用于服务端工作
+
+void TcpServer_Del(TcpServer * server);    //销毁服务端
+
+#endif
