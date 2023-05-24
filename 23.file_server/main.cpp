dd5c0ae35b3d48ecf28b3d4f9a8c644a1f798d60
WONGZEONJYU <958323726@qq.com>
2023-05-25 00:01:58 +0800

    add

--- 23.file_server/main.cpp
+++ 23.file_server/main.cpp
@@ -30,39 +30,74 @@ enum {
 static volatile const char *g_root {};
 static volatile int g_status {};
 
-static void DoResp(void * arg)
+static void DoResp(TcpClient * client)
 {
-    cout << __FUNCTION__ << " : " << arg << endl;
+    int len {TcpClient_Available(client)};
+
+    if (len > 0){
+
+        char * buf {reinterpret_cast<char*>(malloc(len + 1))};
+
+        TcpClient_RecvRaw(client,buf,len);
+
+        buf[len] = 0;
+
+        cout << __FUNCTION__  << " - Request buf : " << buf << endl;
+
+        char resp[255] {};
+
+        sscanf(buf,"GET %s HTTP",resp);
+
+        std::cout << __FUNCTION__ <<" - Req String :" << resp << std::endl;
+
+        const char *format {"HTTP/1.1 200 OK\r\n"
+                        "Server:W.Z.J Http Server\r\n"
+                        "Connect-Length:%d\r\n"
+                        "Connect-Type:text/html\r\n"
+                        "Connection:close\r\n"
+                        "\r\n"
+                        "%s"};
+
+        sprintf(resp,format,strlen((const char *)g_root),g_root);
+        
+        TcpClient_SendRaw(client,resp,strlen(resp));
+
+        TcpClient_Close(client);
+
+        free(buf);
+    }
 }
 
-static void *Client_Thread(void * arg)
+static void *Process_Thread(void * arg)
 {
     while (TcpClient_IsValid(arg)){
         DoResp(arg);
     }
+
+    cout << __FUNCTION__  << "  -  Thread Exit : " << arg << endl;
 
     return arg;
 }
 
 static void * Server_Thread(void * arg)
 {
-    cout << __FUNCTION__ << " : " << arg << endl;
-
     while (TcpServer_IsValid(arg)){
 
         TcpClient * client {TcpServer_Accept(arg)};
 
-        cout << __FUNCTION__ << " : " <<  client << endl;
-
         if (client && (PAUSE != g_status)){
 
             pthread_t tid {};
-            pthread_create(&tid,nullptr,Client_Thread,client);
+            pthread_create(&tid,nullptr,Process_Thread,client);
 
         }else {
             TcpClient_Del(client);
         }
     }
+
+    g_status = STOP;
+
+    //cout << __FUNCTION__  << "  -  Thread Exit : " << arg << endl;
 
     return arg;
 }
@@ -81,14 +116,32 @@ static void Start_Handler(const char *arg)
 
             pthread_t tid {};
             err = pthread_create(&tid,nullptr,Server_Thread,server);
-            cout << "err = " << err << endl;
+
+        }else {
+
+            err = 1;
         }
+    }
+
+    if (!err){
+        g_status = RUN;
+        std::cout << "Server is OK!" << std::endl;
+    }else {
+        g_status = STOP;
+        std::cout << "Server is failed!" << std::endl;
     }
 }
 
 static void Pause_Handler(const char *arg)
 {
+    if(RUN == g_status){
 
+        g_status = PAUSE;
+
+        std::cout << "Server is paused!" << std::endl;
+    }else{
+        std::cout << "Server is NOT started!" << std::endl;
+    }
 }
 
 static void Exit_Handler(const char *arg)
