76a743f60e9381683b2d5bd5bbcd457c7e431c87
WONGZEONJYU <958323726@qq.com>
2023-05-26 18:00:55 +0800

    add

---
+++ 23.file_server/page.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include "page.h"
+
+struct page
+{
+    
+};
+
+char* ToPageString(const char* path,const char* ts)
+{
+    return nullptr;
+}
+
+Table* CreatTable()
+{
+    return nullptr;
+}
+
+Table* InsterRow(Table *table,RowInfo *info)
+{
+    return nullptr;
+}
+
+char* ToTableString(Table *table)
+{
+    return nullptr;
+}
+
+void FreeTable(Table *table)
+{
+    free(table);
+}
