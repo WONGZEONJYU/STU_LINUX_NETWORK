c4fc8696df4819f0325e2dd6c029ef6861ceb439
WONGZEONJYU <958323726@qq.com>
2023-08-10 19:30:31 +0800

    modified

--- 3.echo_server_client/client.cpp
+++ 3.echo_server_client/client.cpp
@@ -9,43 +9,49 @@
 
 using namespace std;
 
-int main() 
+int main(int argc,char* argv[])
 {
     int sock{socket(PF_INET,SOCK_STREAM,0)};
 
     if (-1 == sock){
-        cout << "socket error" << endl;
+        cout << "socket error\n";
         return -1;
     }
 
     sockaddr_in addr {};
     addr.sin_family = AF_INET;
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    addr.sin_port = htons(8888);
-
-    if ( -1 == connect( sock,reinterpret_cast<sockaddr *>(&addr),sizeof(addr) )){
-        cout << "connect error" << endl;
+    //addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    if (!inet_aton("127.0.0.1",&addr.sin_addr)){
+        cout << "changer error\n";
         return -1;
     }
 
-    cout << "connect success sock :" << sock << endl;
+    /*此处ip是服务端ip,由于是在本机做实验,我选用环回地址*/
+    addr.sin_port = htons(8888);
+
+    if ( -1 == connect( sock,reinterpret_cast<sockaddr *>(&addr),sizeof(addr) )){
+        cout << "connect error\n";
+        return -1;
+    }
+
+    cout << "connect success sock :" << sock << '\n';
 
     for(;;) {
 
         char input[32]{},buf[128]{};
 
-        cout << "Input: " << endl;
+        cout << "Input: \n";
 
         cin >> input;
 
-        int len ( send(sock,reinterpret_cast<const void *>(input),strlen(input) + 1,0) ) ;
+        int len ( send(sock,input,(strlen(input) + 1),0) ) ;
 
         int r ( recv(sock,buf,sizeof(buf),0) );
 
         if (r > 0){
-            cout << "Receive :" << buf << endl;
-        }else{
-            cout << "client quit r :" << r << endl;
+            cout << "Receive :" << buf << '\n';
+        }else{  /* 与服务端断开连接 recv(...) 返回 0 */
+            cout << "client quit r :" << r << '\n';
             break;
         }
     }
--- 3.echo_server_client/server.cpp
+++ 3.echo_server_client/server.cpp
@@ -6,37 +6,55 @@
 #include <unistd.h>
 #include <cstring>
 #include <iostream>
+#include <signal.h>
+#include <type_traits>
+#include <functional>
 
 using namespace std;
 
-int main() 
+int server{-1};
+
+void signal_handler(const int sig,siginfo_t *info, void*)
 {
-    int server {socket(PF_INET,SOCK_STREAM,0)};
+    constexpr char str[] {"exit\n"};
+    write(1,str,sizeof(str));
+    close(server);
+    exit(0);
+}
+
+int main(int argc,char* argv[])
+{
+    server = socket(PF_INET,SOCK_STREAM,0);
 
     if (-1 == server){
-        cout << "server socket error" << endl;
+        cout << "server socket error\n";
         return -1;
     }
 
-    sockaddr_in saddr {};
+    struct sigaction act{};
+    act.sa_flags = SA_RESTART | SA_SIGINFO ;
+    act.sa_sigaction = signal_handler;
 
+    sigaction(SIGINT,&act,nullptr);
+
+    sockaddr_in saddr {};
     saddr.sin_family = AF_INET;
     saddr.sin_addr.s_addr = htonl(INADDR_ANY);//htonl函数把小端转换成大端（网络字节序采用大端）
     saddr.sin_port = htons(8888);
 
     if ( -1 == bind( server,reinterpret_cast<const sockaddr *>(&saddr),sizeof(saddr) ) ){
-        cout << "server bind error" << endl;
+        cout << "server bind error\n";
         return -1;
     }
 
     if ( -1 == listen(server,1) ){
-        cout << "server listen error" << endl;
+        cout << "server listen error\n";
         return -1;
     }
 
-    cout << "server start success" << endl;
+    cout << "server start success\n";
 
-    while (true){
+    for(;;){
 
         sockaddr_in caddr {};
         socklen_t asize {sizeof(caddr)};
@@ -44,28 +62,28 @@ int main()
         int client {accept(server,reinterpret_cast<sockaddr *>(&caddr),&asize)};
 
         if (-1 == client){
-            cout << "client accept error" << endl;
+            cout << "client accept error\n";
             return -1;
         }
 
-        cout << "client :" << client << endl; //client的数值表示系统资源的id
+        cout << "client :" << client << '\n'; //client的数值表示系统资源的id
 
         int r {},len{};
 
         do{
             char buf[32]{};
 
-            r = recv(client,(reinterpret_cast<void *>(buf)),(sizeof(buf)/sizeof(char)),0);
+            r = recv(client,buf,(sizeof(buf)/sizeof(*buf)),0);
 
             if (r > 0){
 
-                cout << "Server Receive :" << buf << endl;
+                cout << "Server Receive :" << buf << '\n';
 
-                if ( strcmp(buf,"quit")){
+                if ( strcmp(buf,"quit") ){
 
-                    len = send(client,reinterpret_cast<const void *>(buf),r,0);
+                    len = send(client,buf,r,0);
 
-                }else{
+                }else{ /*0 == strcmp(...) 跳出do while , 客户端断开连接*/
                    break; 
                 }
             }
