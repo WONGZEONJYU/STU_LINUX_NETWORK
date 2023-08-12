578cdbbf58d8a8583a52b84669c5b38f325b2911
WONGZEONJYU <958323726@qq.com>
2023-08-12 12:19:33 +0800

    add

--- 4.ipv4-addr/ipv4-addr.cpp
+++ 4.ipv4-addr/ipv4-addr.cpp
@@ -9,42 +9,49 @@
 
 using namespace std;
 
-#define YES 1
-
 int main(int argc,char* argv[]) 
 {
-#if 1
-    const unsigned int addr {inet_addr("1.2.3.4")};
-#else
-    const unsigned int addr {inet_addr("D.T.Software")};
-#endif
-    in_addr addr1 {0x09080706};
-    in_addr addr2 {0x05040302};
-
-#if YES
-    char * s1 {strcpy(new char[32],inet_ntoa(addr1))};
-    char * s2 {strcpy(new char[32],inet_ntoa(addr2))};
-#else
-    char* s1 {inet_ntoa(addr1)};
-    char* s2 {inet_ntoa(addr2)};
-#endif
-
-    cout << "s1 = " << s1 << '\n';
-    cout << "s2 = " << s2 << '\n';
-
-    cout << "addr = 0x" << hex << addr << '\n';
-
-    cout << "addr1 = 0x" << hex << addr1.s_addr << '\n';
-    cout << "addr2 = 0x" << hex << addr2.s_addr << '\n';
-
-    in_addr addr3 {};
-    if (inet_aton("1.2.3.4",&addr3)){
-        cout << "addr3 = 0x" << hex << addr3.s_addr << '\n';
+    {
+    #if 1
+        const unsigned int addr {inet_addr("1.2.3.4")};
+    #else
+        const unsigned int addr {inet_addr("D.T.Software")};
+    #endif
+        cout << "addr = 0x" << hex << addr << '\n';
     }
 
+    {
+
+#define YES 1
+        in_addr addr1 {0x09080706};
+        in_addr addr2 {0x05040302};
+#if YES
+        char * s1 {strcpy(new char[32],inet_ntoa(addr1))};
+        char * s2 {strcpy(new char[32],inet_ntoa(addr2))};
+#else
+        char* s1 {inet_ntoa(addr1)};
+        char* s2 {inet_ntoa(addr2)};
+#endif
+        cout << "addr1 = 0x" << hex << addr1.s_addr << '\n';
+        cout << "addr2 = 0x" << hex << addr2.s_addr << '\n';
+        cout << "s1 = " << s1 << '\n';
+        cout << "s2 = " << s2 << '\n';
 #if YES
     delete s1;
     delete s2;
+#endif
+
+    }
+
+    in_addr addr3 {};
+#if 1
+    if (inet_aton("1.2.3.4",&addr3)){
+        cout << "addr3 = 0x" << hex << addr3.s_addr << '\n';
+    }
+#else
+    if (inet_aton("D.T.Software",&addr3)){
+        cout << "addr3 = 0x" << hex << addr3.s_addr << '\n';
+    }
 #endif
 
     return 0;
