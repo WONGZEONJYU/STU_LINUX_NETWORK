ea8e69cefb9dd650affd28748b23fa6edee1d418
WONGZEONJYU <958323726@qq.com>
2023-09-11 13:58:01 +0800

    add & modified

--- 17.udp_Multicast/mul_tx.cpp
+++ 17.udp_Multicast/mul_tx.cpp
@@ -33,13 +33,15 @@ int main(int argc, char *argv[])
 
     socklen_t len{};
 
+#if 1
+
     {
         int ttl{};
         len  = sizeof(ttl);
         getsockopt(server,IPPROTO_IP,IP_MULTICAST_TTL,&ttl,&len);
         cout << "default ttl = " << ttl << '\n';
 
-        //设在TTL的值，路由器转发的次数
+        //设置TTL的值，路由器转发的次数
         ttl = 32;
         len  = sizeof(ttl);
         setsockopt(server,IPPROTO_IP,IP_MULTICAST_TTL,&ttl,len);
@@ -47,29 +49,34 @@ int main(int argc, char *argv[])
     }
 
     {
-        int loop{};//数据是否回环发送回本机 0不发送，非0发送
+        int loop{};//数据是否回环发送回本机 0不发送，非0发送 ,默认非0
         len = sizeof(loop);
         getsockopt(server,IPPROTO_IP,IP_MULTICAST_LOOP,&loop,&len);
         cout << "default loop = " << loop << '\n';
-
-        // loop = 0;
-        // len = sizeof(loop);
-        // setsockopt(server,IPPROTO_IP,IP_MULTICAST_LOOP,&loop,len);
-        // cout << "current loop = " << loop << '\n';
+#if 0
+        /*把loop设置为0,本地不接收回环数据*/
+        loop = 0;
+        len = sizeof(loop);
+        setsockopt(server,IPPROTO_IP,IP_MULTICAST_LOOP,&loop,len);
+        cout << "current loop = " << loop << '\n';
+#endif
     }
 
     {    
         in_addr addr;
-        // len = sizeof(addr);
-        // getsockopt(server,IPPROTO_IP,IP_MULTICAST_IF,&addr,&len);
-        // cout << "ip = " << inet_ntoa(addr) << '\n';
-
-        addr.s_addr = inet_addr("10.211.55.3");//采用那个网卡作为多播的发送网卡
+#if 1
+        len = sizeof(addr);
+        getsockopt(server,IPPROTO_IP,IP_MULTICAST_IF,&addr,&len);
+        cout << "get ip = " << inet_ntoa(addr) << '\n';
+#endif
+        addr.s_addr = inet_addr("10.10.10.126");//采用那个网卡作为多播的发送网卡
         //addr.s_addr = htonl(INADDR_ANY);//多个网卡不建议采用0.0.0.0这个地址，有可能出现接收端无法收到数据的情况
         len = sizeof(addr);
         setsockopt(server,IPPROTO_IP,IP_MULTICAST_IF,&addr,len);
-        cout << "ip = " << inet_ntoa(addr) << '\n';
+        cout << "set ip = " << inet_ntoa(addr) << '\n';
     }
+
+#endif
 
     sockaddr_in remote {};
     remote.sin_family = AF_INET;
